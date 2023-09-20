85593b68252894e8dd3ea520d734c8ff71f37480
LyokoJeremie <lucheng989898@gmail.com>
2023-09-21 00:29:52 +0800

    mod

---
+++ src/mod/init.ts
@@ -0,0 +1,8 @@
+import {ModStart} from '../GreasemonkeyScript/start';
+
+(async () => {
+    window.jQuery(document).on(":passageend", () => {
+        const ms = new ModStart(window);
+        ms.initMod();
+    });
+})();
